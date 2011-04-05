53c8a4e3eb47cd1521742a31a73be5941b4a267d
Eevee <git@veekun.com>
2011-04-04 23:32:49 -0700

    Kool-aid part 2: Service class for core logic, and deferreds.

--- dywypi.tac
+++ dywypi.tac
@@ -3,14 +3,32 @@
 
 import os
 from twisted.application import service, internet
+from twisted.internet import defer
 from twisted.web import static, server
 
 from dywypi.core import DywypiFactory, connection_specs
+from dywypi.plugin_api import PluginRegistry
 
-master_service = service.MultiService()
+class DywypiService(service.MultiService):
+    def __init__(self):
+        service.MultiService.__init__(self)
+
+        self.plugin_registry = PluginRegistry()
+        self.plugin_registry.discover_plugins()
+        self.plugin_registry.load_plugin('echo')
+        self.plugin_registry.load_plugin('fyi')
+
+    def dispatch_event(self, responder, command, args):
+        d = defer.maybeDeferred(
+            self.plugin_registry.run_command, command, args)
+        d.addCallback(responder)
+        d.addErrback(lambda failure: responder(u"resonance cascade imminent, evacuate immediately"))
+
+master_service = DywypiService()
 
 for host, port, channels in connection_specs:
-    internet.TCPClient(host, port, DywypiFactory(channels)).setServiceParent(master_service)
+    factory = DywypiFactory(master_service, channels)
+    internet.TCPClient(host, port, factory).setServiceParent(master_service)
 
 application = service.Application("dywypi")
 master_service.setServiceParent(application)
--- dywypi/core.py
+++ dywypi/core.py
@@ -8,15 +8,11 @@ from twisted.internet import reactor, protocol
 from twisted.python import log
 from twisted.words.protocols import irc
 
-from dywypi.plugin_api import PluginRegistry
-
 nickname = 'dywypi2_0'
 connection_specs = [
     ('irc.veekun.com', 6667, ['#bot']),
 ]
 encoding = 'utf8'
-plugin_registry = None
-
 
 class DywypiClient(irc.IRCClient):
     nickname = nickname
@@ -53,15 +49,21 @@ class DywypiClient(irc.IRCClient):
         # Split the command into words, using shell-ish syntax
         tokens = [token.decode(encoding) for token in shlex.split(command)]
 
+        if context == 'channel':
+            def respond(response):
+                self.msg(channel, self._encode(u"{0}: {1}".format(user.nickname, response)))
+        else:
+            def respond(response):
+                self.msg(
+                    self._encode(user.nickname),
+                    self._encode(response),
+                )
+
         # XXX this will become 'respond to an event' I guess.  needs a concept
         # of an event.  right now we have "string of words is directed at bot"
-        response = plugin_registry.run_command(tokens[0], tokens[1:])
-        if context == 'channel':
-            self.msg(channel, u"{0}: {1}".format(user.nickname, response).encode('utf8'))
-        else:
-            self.msg(user.nickname.encode('utf8'), response.encode('utf8'))
+        response = self.factory.service.dispatch_event(
+            responder=respond, command=tokens[0], args=tokens[1:])
 
-        # Done
         return
 
     ### INTERNAL METHODS
@@ -71,6 +73,12 @@ class DywypiClient(irc.IRCClient):
         # TODO  :(  at least implement xchat-style "utf8 or latin1"
         return s.decode('utf8')
 
+    def _encode(self, s):
+        """Returns `s`, encoded into what is probably the channel's encoding.
+        """
+        # TODO encoding per channel?
+        return s.encode('utf8')
+
     user_tuple = namedtuple('user_tuple', ['nickname', 'ident', 'host'])
     def _parse_user(self, raw_user):
         """Parses a user identifier like a!b@c and returns a namedtuple of
@@ -78,6 +86,8 @@ class DywypiClient(irc.IRCClient):
 
         Also takes care of the decoding.
         """
+        # XXX is the username actually supposed to be decoded?  does it have to
+        # be ascii?
         log.err(raw_user)
         nickname, remainder = self._decode(raw_user).split('!', 2)
         ident, host = remainder.split('@', 2)
@@ -87,21 +97,6 @@ class DywypiClient(irc.IRCClient):
 class DywypiFactory(protocol.ReconnectingClientFactory):
     protocol = DywypiClient
 
-    def __init__(self, channels):
+    def __init__(self, service, channels):
+        self.service = service
         self.channels = channels
-
-
-if __name__ == '__main__':
-    # XXX uhh should probably make this an Application and run under twistd,
-    # then add the registry to that
-    plugin_registry = PluginRegistry()
-    plugin_registry.discover_plugins()
-    plugin_registry.load_plugin('echo')
-    plugin_registry.load_plugin('fyi')
-
-    connections = []
-    for host, port, channels in connection_specs:
-        reactor.connectTCP(host, port, DywypiFactory(channels))
-
-    log.startLogging(sys.stdout)
-    reactor.run()
