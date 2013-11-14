ba044ce480b0b5d8d1a0f74c4d3fbf47d437b91b
Eevee (Alex Munroe) <eevee.git@veekun.com>
2013-11-14 11:33:15 -0800

    Initial commit: sort of works!

---
+++ dywypi/__main__.py
@@ -0,0 +1,17 @@
+import asyncio
+import sys
+
+from dywypi.aioirc import IRCClient
+
+
+@asyncio.coroutine
+def main(loop, host, port, password):
+    tr, pr = yield from loop.create_connection(lambda: IRCClient(password), host, port, ssl=True)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    # TODO i think this blocks until the connection is made, which seems...  not
+    # right?  is there a better way to actually get started?
+    loop.run_until_complete(main(loop, *sys.argv[1:]))
+    loop.run_forever()
---
+++ dywypi/aioirc.py
@@ -0,0 +1,107 @@
+import asyncio
+import re
+
+
+
+class IRCClient(asyncio.Protocol):
+    def __init__(self, password):
+        self.password = password
+
+        self.buf = b''
+        self.registered = False
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print("connection made!!")
+
+        # TODO maybe stick this bit in a coroutine?
+        self.send_message('PASS', self.password)
+        self.send_message('NICK', 'dywypi')
+        self.send_message('USER', 'dywypi', '-', '-', 'dywypi Python IRC bot')
+
+    def data_received(self, data):
+        data = self.buf + data
+        while True:
+            raw_message, delim, data = data.partition(b'\r\n')
+            if not delim:
+                # Incomplete message; stop here and wait for more
+                self.buf = raw_message
+                return
+
+            # TODO valerr
+            message = Message.parse(raw_message.decode('utf8'))
+            print("recv:", repr(message))
+            self.handle_message(message)
+
+    def handle_message(self, message):
+        if message.command == 'PING':
+            self.send_message('PONG', message.args[-1])
+            if not self.registered:
+                self.registered = True
+                self.send_message('JOIN', '#dywypi')
+
+    def send_message(self, command, *args):
+        message = Message(command, *args)
+        print("sent:", repr(message))
+        self.transport.write(message.render().encode('utf8') + b'\r\n')
+
+
+class Message:
+    def __init__(self, command, *args, prefix=None):
+        # TODO command can't be a number when coming from a client
+        self.command = command
+        self.prefix = prefix
+        self.args = args
+
+        # TODO stricter validation: all str (?), last arg...
+
+    def __repr__(self):
+        return "<{name}: {command} {args}>".format(
+            name=type(self).__name__,
+            command=self.command,
+            args=', '.join(repr(arg) for arg in self.args),
+        )
+
+    def render(self):
+        parts = [self.command] + list(self.args)
+        # TODO assert no spaces
+        # TODO assert nothing else begins with colon!
+        if self.args and ' ' in parts[-1]:
+            parts[-1] = ':' + parts[-1]
+
+        return ' '.join(parts)
+
+    PATTERN = re.compile(
+        r'''\A
+        (?: : (?P<prefix>[^ ]+) [ ]+ )?
+        (?P<command> \d{3} | [a-zA-Z]+ )
+        (?P<args>
+            (?: [ ]+ [^: \x00\r\n][^ \x00\r\n]* )*
+        )
+        (?:
+            [ ]+ [:] (?P<trailing> [^\x00\r\n]*)
+        )?
+        [ ]*
+        \Z''',
+        flags=re.VERBOSE)
+
+    @classmethod
+    def parse(cls, string):
+        # TODO uhhh what happens with encodings here.  ascii command, arbitrary
+        # encoding for everything else?
+        m = cls.PATTERN.match(string)
+        if not m:
+            raise ValueError(repr(string))
+
+        argstr = m.group('args').lstrip(' ')
+        if argstr:
+            args = re.split(' +', argstr)
+        else:
+            args = []
+
+        if m.group('trailing'):
+            args.append(m.group('trailing'))
+
+        return cls(m.group('command'), *args, prefix=m.group('prefix'))
+
+
